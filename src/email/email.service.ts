48eebf09ce5be850ebfc92d0c70b5c3c70fdb87a
onurutku <topaloglu08@gmail.com>
2023-04-08 00:00:44 +0300

    rebuild

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -5,12 +5,12 @@ import { MailerService } from '@nestjs-modules/mailer';
 export class EmailService {
   constructor(private mailService: MailerService) {}
   async sendVerificationEmail(mailTo: string) {
-    await this.mailService.sendMail({
+    const result = await this.mailService.sendMail({
       to: mailTo,
       from: 'barinakdeneme@gmail.com',
       subject: 'Verify your email address',
       html: `<a href='https://barinak.herokuapp.com/auth/verify-user?email=${mailTo}'>Click here to verify your email</a>`,
     });
-    return 'Please check your mailbox to verify your account';
+    return result;
   }
 }
