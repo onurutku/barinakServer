bbbbbfa837075f0c5f91a97eec5d114f54b142a7
Onur Utku TOPALOĞLU <47296136+onurutku@users.noreply.github.com>
2023-04-08 01:58:59 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'dbinfo',
-    ),
     AuthModule,
     EmailModule,
   ],
